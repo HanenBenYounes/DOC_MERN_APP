05c56f97ded75dadf48b98856e9f0dc05fcdb99f
hanenBenyounes <hanen.benaliessalah@gmail.com>
2023-07-21 07:53:46 +0100

    new commit

--- models/userModels.js
+++ models/userModels.js
@@ -13,8 +13,20 @@ const userSchema =new mongoose.Schema({
         type: String, 
         required:[true, 'password is required']
     },
+    isDoctor:{
+        type:Boolean,
+        default:false,
+    },
+    notification:{
+        type:Array,
+        default:[]
+    },
+    seennotification:{
+        type: Array,
+        default:[],
+    },
 
 
-})
-const userModel= mongoose.model ('users', userSchema)
-module.exports= userModel
+});
+const userModel= mongoose.model ('users', userSchema);
+module.exports= userModel;
